dc05274ca782cf797efb7812d5b9195258a5d934
cybertino <istodim@gmail.com>
2024-05-11 14:46:51 +0930

    Setup Typescript, ESLint and Prettier

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'plugin:prettier/recommended',
+    'prettier',
+    'eslint:recommended'
+  ],
+  plugins: ['@typescript-eslint'],
+  env: {
+    node: true,
+  }
+};
--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# lyka
+# Gooddy
+
+Gooddy is a robot that manages goods in the warehouse.
+
+## Setup
+
+Install all the dependencies:
+
+```bash
+pnpm install
+```
+
+## Execute
+
+```bash
+pnpm run dev
+```
